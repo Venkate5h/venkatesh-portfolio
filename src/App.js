5cfb027d6defcff2f1997b4f24cdda75fe3a189b
Venkatesh <jampackvenky@gmail.com>
2020-02-02 17:01:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio by
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Venkatesh M
+        </h1>
       </header>
     </div>
   );
